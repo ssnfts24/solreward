46963e9611c19276481dc0793a4571ef03039fc4
ssnfts24 <l.aaronpaul24@gmail.com>
2025-03-04 12:37:45 -0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/sitemap.xml",
-        headers: [
-          {
-            key: "Content-Type",
-            value: "application/xml; charset=utf-8",
-          },
-          {
-            key: "Cache-Control",
-            value: "public, max-age=0, must-revalidate",
-          },
-        ],
-      },
-    ];
-  },
+  headers: () => [
+    {
+      source: "/sitemap.xml",
+      headers: [
+        {
+          key: "Content-Type",
+          value: "application/xml; charset=utf-8",
+        },
+        {
+          key: "Cache-Control",
+          value: "public, max-age=0, must-revalidate",
+        },
+      ],
+    },
+  ],
 };
 
 export default nextConfig;
